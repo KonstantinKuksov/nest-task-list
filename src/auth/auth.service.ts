0174febb5a921ec2578df0d9d2dc4e1decc14bb7
Konstantin Kuksov <k.kuksov@newlinetechnologies.net>
2022-06-30 14:45:07 +0300

    Feat: created signIn method, added validation to username and password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +14,18 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<any> {
+    const { username, password } = authCredentialsDto;
+    const user = this.userRepository.create({ username, password });
+    try {
+      await this.userRepository.save(user);
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
