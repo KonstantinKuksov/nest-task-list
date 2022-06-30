4cc175447e2fccb762f440702270625a30b0268c
Konstantin Kuksov <k.kuksov@newlinetechnologies.net>
2022-06-30 19:42:15 +0300

    Feat: implemented signIn method and password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,13 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -17,7 +19,12 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<any> {
     const { username, password } = authCredentialsDto;
-    const user = this.userRepository.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.userRepository.create({
+      username,
+      password: hashedPassword,
+    });
     try {
       await this.userRepository.save(user);
     } catch (e) {
@@ -26,6 +33,16 @@ export class AuthService {
       } else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ where: { username } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success!';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
     }
   }
 }
